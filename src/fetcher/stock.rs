626aa2d488f25e36bc88c5ce89421a4f2f314cc9
jht6 <juhaotian@foxmail.com>
2024-04-08 05:02:20 +0800

    feat: 定时更新token

--- src/fetcher/stock.rs
+++ src/fetcher/stock.rs
@@ -1,9 +1,14 @@
 use crate::common::consts::req::HEADER_USER_AGENT;
+use chrono::Local;
+use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
-use std::io;
-
-// const STOCK_API: &str = "https://stock.xueqiu.com/v5/stock/realtime/quotec.json?symbol=";
+use std::{io, sync::Mutex, thread, time::Duration};
 const STOCK_API: &str = "https://stock.xueqiu.com/v5/stock/batch/quote.json?symbol=";
+const FETCH_TOKEN_INTERVAL_SECS: u64 = 60;
+
+lazy_static! {
+    static ref XQ_TOKEN: Mutex<String> = Mutex::new("".to_string());
+}
 
 // ------------------------雪球API json结构----------------------------
 #[derive(Serialize, Deserialize, Debug)]
@@ -34,23 +39,32 @@ pub struct Quote {
 
 #[derive(Clone)]
 pub struct StockFetcher {
-    xq_token: String,
+    // xq_token: String,
 }
 
 impl StockFetcher {
     pub async fn new() -> Self {
-        let mut fetcher = Self {
-            xq_token: "".to_string(),
-        };
+        let fetcher = Self {};
 
         // 直接解包,若有错误直接panic
-        fetcher.init().await.unwrap();
+        Self::update_token().await.unwrap();
+
+        tokio::task::spawn(async {
+            loop {
+                thread::sleep(Duration::from_secs(FETCH_TOKEN_INTERVAL_SECS));
+                Self::update_token().await.unwrap();
+            }
+        });
 
         fetcher
     }
 
-    async fn init(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        // 获取cookie的token值
+    fn set_token(token: String) {
+        let mut t = XQ_TOKEN.lock().unwrap();
+        *t = token;
+    }
+
+    async fn fetch_token() -> Result<String, Box<dyn std::error::Error>> {
         let res = reqwest::Client::new()
             .get("https://xueqiu.com/")
             .header("User-Agent", HEADER_USER_AGENT)
@@ -61,8 +75,31 @@ impl StockFetcher {
             let s = v.to_str().unwrap();
             if s.contains("xq_a_token") {
                 let list: Vec<&str> = s.split(";").collect();
-                self.xq_token = list[0].to_string();
-                break;
+                return Ok(list[0].to_string());
+            }
+        }
+
+        Err(Box::new(io::Error::new(
+            io::ErrorKind::Other,
+            "Cannot get xq_token",
+        )))
+    }
+
+    pub async fn update_token() -> Result<(), Box<dyn std::error::Error>> {
+        let token = Self::fetch_token().await.unwrap();
+        let old_token = XQ_TOKEN.lock().unwrap().clone();
+
+        let now = Local::now();
+
+        if token != old_token {
+            Self::set_token(token.clone());
+
+            if old_token != "" {
+                println!(
+                    "{} xq_token is changed: {}",
+                    now.format("%Y-%m-%d %H:%M:%S"),
+                    &token
+                );
             }
         }
 
@@ -71,6 +108,10 @@ impl StockFetcher {
 
     // 参考 https://github.com/LeekHub/leek-fund/blob/master/src/explorer/stockService.ts#L382
     pub async fn get_data(&self, stock_code: &str) -> Result<Quote, Box<dyn std::error::Error>> {
+        let t = XQ_TOKEN.lock().unwrap();
+        let token = t.clone();
+        drop(t); // 释放锁
+
         let json_data = reqwest::Client::new()
             .get(STOCK_API.to_owned() + stock_code)
             .header("User-Agent", HEADER_USER_AGENT.to_string())
@@ -81,7 +122,7 @@ impl StockFetcher {
                 "en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3",
             )
             .header("Referer", "https://stock.xueqiu.com/")
-            .header("Cookie", self.xq_token.to_string())
+            .header("Cookie", token)
             .send()
             .await?
             .json::<ResJson>()
